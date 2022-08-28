36324dd7ee2e9f8c5810081f74f714682fe2cf82
-CX- <nz.carterxiao@gmail.com>
2022-08-29 09:23:06 +1200

    core

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,47 +1,232 @@
 import * as React from 'react';
 import '../styles/ui.css';
+import firstNames from './FirstNames';
+import lastNames from './LastNames';
 
 declare function require(path: string): any;
 
 const App = ({}) => {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
-
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
+    React.useEffect(() => {
+        parent.postMessage({pluginMessage: {type: 'run'}}, '*');
     }, []);
 
-    const onCreate = () => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-    };
+    const textbox = React.useRef<HTMLInputElement>(undefined);
 
-    const onCancel = () => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    };
+    const options = [
+        'First name',
+        'Last name',
+        'Full name',
+        'City',
+        'Age',
+        'Address',
+        'Phone number',
+        'Email',
+        'Password',
+        'ID',
+    ];
+
+    //usestate for message
+    const [layers, setLayers] = React.useState([]);
+    const [groupByLayerName, setGroupByLayerName] = React.useState(false);
+    const [insertOrder, setInsertOrder] = React.useState('random');
+    const [userData, setUserData] = React.useState('');
+    const [selectedOption, setSelectedOption] = React.useState(options[0]);
 
     React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
         window.onmessage = (event) => {
             const {type, message} = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
+            if (type === 'selection') {
+                // console.log(message.textLayers);
+                setLayers(message.textLayers);
             }
         };
     }, []);
 
+    //get the unique occurences of the layer name and put in array
+    const getUniqueLayerNames = () => {
+        const uniqueLayerNames = [];
+        layers.forEach((layer) => {
+            if (!uniqueLayerNames.includes(layer.name)) {
+                uniqueLayerNames.push(layer.name);
+            }
+        });
+        return uniqueLayerNames;
+    };
+
+    const genRandomFullNames = () => {
+        return (
+            firstNames[Math.floor(Math.random() * firstNames.length)] +
+            ' ' +
+            lastNames[Math.floor(Math.random() * lastNames.length)]
+        );
+    };
+
+    const updateText = (layer, e) => {
+        layer.value = e.target.value;
+        setLayers([...layers]);
+        parent.postMessage({pluginMessage: {type: 'overwrite', result: layers}}, '*');
+    };
+
     return (
         <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
+            <h2>Hello world</h2>
+            <button
+                onClick={() => {
+                    parent.postMessage({pluginMessage: {type: 'run'}}, '*');
+                }}
+            >
+                Refresh
             </button>
-            <button onClick={onCancel}>Cancel</button>
+            {/* checkbox */}
+            <label>
+                <input
+                    type="checkbox"
+                    checked={groupByLayerName}
+                    onChange={() => setGroupByLayerName(!groupByLayerName)}
+                />
+                Group by layer name
+            </label>
+
+            {/* show button and input only when groupByLayerName is false */}
+            {!groupByLayerName && (
+                <div>
+                    <p>overwrite all selected texts</p>
+                    <input
+                        type="text"
+                        ref={textbox}
+                        onChange={(e) => {
+                            const {value} = e.target;
+                            const updatedLayers = layers.map((layer) => {
+                                return {
+                                    ...layer,
+                                    value,
+                                };
+                            });
+
+                            setLayers(updatedLayers);
+                            parent.postMessage({pluginMessage: {type: 'overwrite', result: updatedLayers}}, '*');
+                        }}
+                    />
+
+                    <button
+                        onClick={() => {
+                            const updatedLayers = layers.map((layer) => {
+                                return {
+                                    ...layer,
+                                    value: genRandomFullNames(),
+                                };
+                            });
+                            setLayers(updatedLayers);
+                            parent.postMessage({pluginMessage: {type: 'overwrite', result: updatedLayers}}, '*');
+                        }}
+                    >
+                        Random
+                    </button>
+                </div>
+            )}
+
+            {/* if groupbylayername is true, then group by layer element by getUniqueLayerNames() */}
+            {groupByLayerName
+                ? getUniqueLayerNames().map((layerName) => {
+                      return (
+                          <>
+                              <p>{layerName}</p>
+                              <p>overwrite all selected texts</p>
+                              <input
+                                  type="text"
+                                  ref={textbox}
+                                  onChange={(e) => {
+                                      const {value} = e.target;
+                                      const updatedLayers = layers.map((layer) => {
+                                          if (layer.name === layerName) {
+                                              return {
+                                                  ...layer,
+                                                  value,
+                                              };
+                                          } else {
+                                              return layer;
+                                          }
+                                      });
+
+                                      setLayers(updatedLayers);
+                                      parent.postMessage(
+                                          {pluginMessage: {type: 'overwrite', result: updatedLayers}},
+                                          '*'
+                                      );
+                                  }}
+                              />
+                              <button
+                                  onClick={() => {
+                                      const updatedLayers = layers.map((layer) => {
+                                          if (layer.name === layerName) {
+                                              console.log(layer.id);
+                                              return {
+                                                  ...layer,
+                                                  value: genRandomFullNames(),
+                                              };
+                                          } else {
+                                              return layer;
+                                          }
+                                      });
+                                      setLayers(updatedLayers);
+                                      parent.postMessage(
+                                          {pluginMessage: {type: 'overwrite', result: updatedLayers}},
+                                          '*'
+                                      );
+                                  }}
+                              >
+                                  Random
+                              </button>
+                              <Dropdown
+                                  options={options}
+                                  selectedOption={selectedOption}
+                                  setSelectedOption={setSelectedOption}
+                              />
+                              <div key={layerName}>
+                                  {layers
+                                      .filter((layer) => layer.name === layerName)
+                                      .map((layer) => {
+                                          return (
+                                              <div key={layer.id}>
+                                                  <input
+                                                      type="text"
+                                                      ref={textbox}
+                                                      value={layer.value}
+                                                      onChange={(e) => updateText(layer, e)}
+                                                  />
+                                              </div>
+                                          );
+                                      })}
+                              </div>
+                          </>
+                      );
+                  })
+                : layers.map((layer) => {
+                      return (
+                          <div key={layer.id}>
+                              <p>{layer.name}</p>
+                              <input
+                                  type="text"
+                                  ref={textbox}
+                                  value={layer.value}
+                                  onChange={(e) => updateText(layer, e)}
+                              />
+                          </div>
+                      );
+                  })}
         </div>
+    );
+};
+
+const Dropdown = ({options, selectedOption, setSelectedOption}) => {
+    return (
+        <select onChange={(e) => setSelectedOption(e.target.value)} value={selectedOption}>
+            {options.map((option) => (
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
     );
 };
 
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,75 @@
-figma.showUI(__html__);
+figma.showUI(__html__, {width: 300, height: 600});
+
+let textLayers = [];
+let allLayers = [];
+let selection = figma.currentPage.selection;
+
+figma.on('selectionchange', () => {
+    // getSelections();
+});
+
+const getSelections = () => {
+    selection = figma.currentPage.selection;
+    textLayers = [];
+    allLayers = [];
+    lookingForText(selection);
+    console.log('text layers: ' + textLayers);
+    console.log('all layers: ' + allLayers);
+    figma.ui.postMessage({
+        type: 'selection',
+        message: {
+            textLayers: textLayers,
+        },
+    });
+};
+
+//recursion function look for text layers in the selection and selection children
+const lookingForText = (element) => {
+    console.log(element);
+    //if element is array, then loop through element
+    if (Array.isArray(element)) {
+        console.log(1);
+        element.forEach((element) => {
+            lookingForText(element);
+        });
+    } else {
+        //if element is a text layer, then return the text
+        if (element.type === 'TEXT') {
+            console.log(2);
+            textLayers.push({
+                id: element.id,
+                name: element.name,
+                value: element.characters,
+            });
+            allLayers.push(element);
+        } else if (element.children !== undefined && element.children.length > 0) {
+            console.log(3);
+            lookingForText(element.children);
+        } else if (element.children === undefined) {
+            console.log(4);
+            allLayers.push(element);
+        }
+    }
+};
 
 figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-
-        // This is how figma responds back to the ui
-        figma.ui.postMessage({
-            type: 'create-rectangles',
-            message: `Created ${msg.count} Rectangles`,
-        });
+    if (msg.type === 'run') {
+        getSelections();
     }
 
-    figma.closePlugin();
+    if (msg.type === 'overwrite') {
+        // console.log(msg.newObj)
+        const updated = msg.result;
+        //find the current page for node with updated[i].id, then change the text to updated[i].value
+        updated.forEach((element) => {
+            // const node = figma.currentPage.findOne(node => node.id === element.id)
+            //element id = all layers id
+            const node = allLayers.find((node) => node.id === element.id);
+            console.log('overwriting...');
+            figma.loadFontAsync({family: node.fontName.family, style: node.fontName.style}).then(() => {
+                node.characters = element.value;
+            });
+        });
+    }
+    // figma.closePlugin();
 };
